431ea9c7098dfcea46a57404541a34847559ddba
Shotaro Ooki <shoutaro0726@gmail.com>
2020-05-31 02:27:40 +0900

    Commit

---
+++ api/views.py
@@ -0,0 +1,15 @@
+from rest_framework import viewsets
+from django.http import HttpResponse
+from .serializers import DeviceSerializer
+from .models import Device
+
+class DeviceViewSet(viewsets.ModelViewSet):
+    queryset = Device.objects.all()
+    serializer_class = DeviceSerializer
+
+    def post(self, request, *args, **kwargs):
+        img = request.data['img']
+        title = request.data['title']
+        Device.objects.create(title=title, img=img)
+        return HttpResponse({'message': 'NewDeviceCreated'}, status=200)
+        
